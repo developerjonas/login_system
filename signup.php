52c0ae3c70c969f7255b9e60d01fd377b8abcd78
developerjonas <thedeveloperjonas@gmail.com>
2024-10-19 12:54:12 +0545

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,4 +1,5 @@
 <?php
+// here it is assumed that you have already created a user database table name `users`
 require 'db.php';
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['name']) && isset($_POST['email']) && isset($_POST['username']) && isset($_POST['pass'])) {
     $name = $_POST['name'];
